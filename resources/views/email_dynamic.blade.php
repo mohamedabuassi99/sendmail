0d6f80530b5feaa04fe7ff4fe4aced749542dff2
unknown <mohamed.abuassi.99@hotmail.com>
2020-02-07 05:46:01 +0200

    first commit

---
+++ resources/views/email_dynamic.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<p>Hi, This is {{ $data['name'] }}</p>
+<p>I have some query like {{ $data['message'] }}.</p>
+<p>It would be appriciative, if you gone through this feedback.</p>
+</body>
+</html>
